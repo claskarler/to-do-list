e7f6fa655725cece2fd926da8e27c357b2d8d434
Clara <cskarler@gmail.com>
2025-03-15 20:17:10 +0100

    add base functionality

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import ToDoList from './components/To-do-list.vue';
 
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 </script>
 
 <template>
