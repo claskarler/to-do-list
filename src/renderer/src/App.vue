b4738699d0dc8718900b2c177e7ef6f8b97a07db
Clara <cskarler@gmail.com>
2025-03-15 19:31:12 +0100

    first commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ToDoList from './components/To-do-list.vue';
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <To-do-list />
+</template>
